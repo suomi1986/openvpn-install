51cb7750dec93a3dc4763804b962cdc578a103e8
suomi1986 <105849054+suomi1986@users.noreply.github.com>
2025-01-29 06:53:44 +0300

    Update openvpn-install.sh
    das

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -4,7 +4,7 @@
 #
 # Copyright (c) 2013 Nyr. Released under the MIT License.
 
-
+ 
 # Detect Debian users running the script with "sh" instead of bash
 if readlink /proc/$$/exe | grep -q "dash"; then
 	echo 'This installer needs to be run with "bash", not "sh".'
